a29c04012c5bfb643063f66aab712b5513676aec
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-04-11 03:58:47 +0600

    add validation on registration page

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,7 +1,25 @@
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 
+
 const Registration = () => {
+    const {
+        register,
+        handleSubmit,
+        
+        formState: { errors },
+    } = useForm()
+
+    const onSubmit = (data) => {
+        // console.log(data)
+        const { name, email, image, password } = data;
+        console.log(password);
+
+    
+    }
+
+
     return (
         <div className="min-h-[calc(100vh-337px)] flex justify-center items-center">
             <div className="w-full md:w-4/6 lg:w-3/6 mx-auto p-8 space-y-3 rounded-xl dark:bg-gray-50 dark:text-gray-800">
@@ -10,22 +28,46 @@ const Registration = () => {
                     <p className="text-xl text-center mx-auto md:w-3/4 ">Welcome to our platform! Please fill out the following information to create your account. Your security is our top priority, so please ensure that you provide accurate information.
                     </p>
                 </div>
-                <form noValidate="" action="" className="space-y-6 bg-[#d17e9b71] ring-1 ring-purple-500 rounded-lg px-10 py-5">
+                <form onSubmit={handleSubmit(onSubmit)} className="space-y-6 bg-[#d17e9b71] ring-1 ring-purple-500 rounded-lg px-10 py-5">
                     <div className="space-y-1 text-lg">
                         <label htmlFor="Text" className="block  dark:text-gray-600">Name</label>
-                        <input type="text" id="Text" placeholder="Type Your Email" className="ring-1 focus:ring-2 focus:ring-stone-950 ring-indigo-500 input input-bordered w-full" />
+                        <input {...register("name", { required: true})} type="text" id="name" placeholder="Type Your Name" className="ring-1 focus:ring-2 focus:ring-stone-950 ring-indigo-500 input input-bordered w-full" />
+                        {errors.name && <span className="text-red-500">This field is required</span>}
+                        
                     </div>
                     <div className="space-y-1 text-lg">
                         <label htmlFor="email" className="block  dark:text-gray-600">Email</label>
-                        <input type="email" id="email" placeholder="Type Your Email" className="focus:ring-2 focus:ring-stone-950 input ring-1 ring-indigo-500 input-bordered w-full" />
+                        <input {...register("email", { required: true })} type="email" id="email" placeholder="Type Your Email" className="focus:ring-2 focus:ring-stone-950 input ring-1 ring-indigo-500 input-bordered w-full" />
+                        {errors.email && <span className="text-red-500">This field is required</span>}
                     </div>
                     <div className="space-y-1 text-lg">
                         <label htmlFor="image" className="block dark:text-gray-600">Profile Image </label>
-                        <input type="text" id="image" placeholder="Your Image Link" className="input focus:ring-2 focus:ring-stone-950 ring-1 ring-indigo-500 input-bordered w-full" />
+                        <input {...register("image", { required: true })} type="text" id="image" placeholder="Your Image Link" className="input focus:ring-2 focus:ring-stone-950 ring-1 ring-indigo-500 input-bordered w-full" />
+                        {errors.image && <span className="text-red-500">This field is required</span>}
                     </div>
                     <div className="space-y-1 text-lg">
                         <label htmlFor="password" className="block dark:text-gray-600">Password</label>
-                        <input type="password" id="password" placeholder="Type Your Email" className="input focus:ring-2 focus:ring-stone-950 ring-1 ring-indigo-500 input-bordered w-full" />
+                        <input {...register("password", {
+                            required: true,
+                            validate: {
+                                minLength: value => value.length >= 6,
+                                uppercase: value => /[A-Z]/.test(value),
+                                lowercase: value => /[a-z]/.test(value)
+                            }
+                        
+                        })} type="password" id="password" placeholder="Type Your Email" className="input focus:ring-2 focus:ring-stone-950 ring-1 ring-indigo-500 input-bordered w-full" />
+                        {errors.password && errors.password.type === "required" && (
+                            <span className="text-red-500">This field is required</span>
+                        )}
+                        {errors.password && errors.password.type === "minLength" && (
+                            <p className="text-red-500">Password must be at least 6 characters long</p>
+                        )}
+                        {errors.password && errors.password.type === "uppercase" && (
+                            <p className="text-red-500">Password must contain at least one capital letter</p>
+                        )}
+                        {errors.password && errors.password.type === "lowercase" && (
+                            <p className="text-red-500">Password must contain at least one lowercase letter</p>
+                        )}
                     </div>
                     <button type="submit" className="block text-xl font-medium text-white bg-[#4556ebea] hover:text-black hover:bg-[#4652c081] py-4 rounded-xl w-full">Login</button>
                 </form>
